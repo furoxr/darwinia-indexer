ca2f5ff6c26f737655ce7d8121cdf9e7997a4025
nada <37610161+furoxr@users.noreply.github.com>
2023-06-26 17:04:35 +0800

    Init

---
+++ src/chaintypes.ts
@@ -0,0 +1,9 @@
+import { typesBundleForPolkadot } from "@darwinia/types/mix";
+
+export default {
+  typesBundle: {
+    spec: {
+      darwinia: typesBundleForPolkadot.spec.darwinia,
+    },
+  },
+};
